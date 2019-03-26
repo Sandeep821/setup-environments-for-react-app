b505510ed4189edcb8ddc259ee2656d72a3a2d08
Soni <sonis@vwcredit.com>
2019-03-26 17:28:16 -0500

    setting environment variables for dev, int, cons and prod

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>this app is running in <strong>{process.env.REACT_APP_ENV}</strong> environment and can call <strong>{process.env.REACT_APP_BASE_API_URL}</strong></p>
           <a
             className="App-link"
             href="https://reactjs.org"
